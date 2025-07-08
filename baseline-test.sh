c86d6ef4a50258db04f63370bea627b96f729b12
Yesika Ramirez <yramirezcard@yramirezcard-mlt.client.nvidia.com>
2025-07-08 10:56:42 +0200

    add script

---
+++ baseline-test.sh
@@ -0,0 +1,70 @@
+
+
+genai-perf profile\
+    -m $MODEL \
+    --endpoint-type chat \
+    --service-kind openai \
+    --streaming \
+    -u $ENDPOINT \
+    --synthetic-input-tokens-mean $INPUT_SEQUENCE_LENGTH \
+    --synthetic-input-tokens-stddev $INPUT_SEQUENCE_STD \
+    --concurrency $CONCURRENCY \
+    --output-tokens-mean $OUTPUT_SEQUENCE_LENGTH \
+    --extra-inputs max_tokens:$OUTPUT_SEQUENCE_LENGTH \
+    --extra-inputs min_tokens:$OUTPUT_SEQUENCE_LENGTH \
+    --extra-inputs ignore_eos:true \
+    --tokenizer $TOKENIZER \
+    -- \
+    -v \
+    --max-threads=256
+
+declare -A useCases
+
+# Populate the array with use case descriptions and their specified input/output lengths
+useCases["Translation"]="200/200"
+useCases["Text classification"]="200/5"
+useCases["Text summary"]="1000/200"
+useCases["Translation2"]="500/2000"
+useCases["Text classification2"]="1000/1000"
+useCases["Text summary2"]="5000/500"
+# Function to execute genAI-perf with the input/output lengths as arguments
+runBenchmark() {
+    local description="$1"
+    local lengths="${useCases[$description]}"
+    IFS='/' read -r inputLength outputLength <<< "$lengths"
+
+    echo "Running genAI-perf for $description with input length $inputLength and output length $outputLength"
+    #Runs
+    for concurrency in 1 2 5 10; do
+
+        local INPUT_SEQUENCE_LENGTH=$inputLength
+        local INPUT_SEQUENCE_STD=0
+        local OUTPUT_SEQUENCE_LENGTH=$outputLength
+        local CONCURRENCY=$concurrency
+        local MODEL=meta/llama-3.1-8b-instruct
+
+        genai-perf profile \
+            -m $MODEL \
+            --endpoint-type chat \
+            --streaming \
+            -u $ENDPOINT \
+            --synthetic-input-tokens-mean $INPUT_SEQUENCE_LENGTH \
+            --synthetic-input-tokens-stddev $INPUT_SEQUENCE_STD \
+            --concurrency $CONCURRENCY \
+            --output-tokens-mean $OUTPUT_SEQUENCE_LENGTH \
+            --extra-inputs max_tokens:$OUTPUT_SEQUENCE_LENGTH \
+            --extra-inputs min_tokens:$OUTPUT_SEQUENCE_LENGTH \
+            --extra-inputs ignore_eos:true \
+            --tokenizer $TOKENIZER \
+            --measurement-interval 10000 \
+            --profile-export-file ${INPUT_SEQUENCE_LENGTH}_${OUTPUT_SEQUENCE_LENGTH}.json \
+            -- \
+            -v \
+            --max-threads=256
+    done
+}
+
+# Iterate over all defined use cases and run the benchmark script for each
+for description in "${!useCases[@]}"; do
+    runBenchmark "$description"
+done
